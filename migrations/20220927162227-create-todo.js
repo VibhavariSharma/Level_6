fd6af05535b8da59fd4379ce60d414579ff204f6
VibhavariSharma <86106525+VibhavariSharma@users.noreply.github.com>
2023-01-04 19:58:57 +0530

    Update 20220927162227-create-todo.js

--- migrations/20220927162227-create-todo.js
+++ migrations/20220927162227-create-todo.js
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Todos');
-  }
+    await queryInterface.dropTable("Todos");
+  },
 };
